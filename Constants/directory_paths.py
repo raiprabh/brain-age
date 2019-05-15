0cc00e8e82e534e66a59768344974e476916365e
Prabhjot Singh Rai <prabhjot@ip-172-31-35-86.us-west-2.compute.internal>
2019-05-15 07:08:39 +0000

    figure out the reasons behind dind discarding data

--- Constants/directory_paths.py
+++ Constants/directory_paths.py
@@ -3,3 +3,11 @@ data_root = '/shared/'
 ixi_root = data_root + 'ixi-dataset/'
 
 ixi_t1_root = ixi_root + 'IXI-T1/'
+
+fcp_root = data_root + 'fcp/'
+
+fcp_data_directories = ['Baltimore', 'Bangor', 'Beijing_Zang', 'Berlin_Margulies', 'Cambridge_Buckner', 'Dallas', 'ICBM', 'Leiden_2180', 'Leiden_2200', 'Leipzig', 'Milwaukee_a', 'Milwaukee_b', 'Munchen', 'Newark', 'NewHaven_a', 'NewHaven_b', 'NewYork_a_ADHD', 'NewYork_a', 'NewYork_b', 'Ontario', 'Orangeburg', 'Oulu', 'Oxford', 'PaloAlto', 'Pittsburgh', 'Queensland', 'SaintLouis', 'Taipei_a', 'Taipei_b', 'Atlanta', 'AnnArbor_a', 'AnnArbor_b']
+
+fcp_data_directories_no_demographics = ['Milwaukee_a', 'Ontario', 'Taipei_a', 'Taipei_b']
+
+fcp_data_directories_with_demographics = list(set(fcp_data_directories) - set(fcp_data_directories_no_demographics))
